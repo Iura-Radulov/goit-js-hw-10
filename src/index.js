d4e65cefb02ad810897b037be577e7a5b8160a83
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-05-12 21:45:14 +0300

    step1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import { fetchCountries } from "./fetchCountries";
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector("#search-box");
+
+searchBox.addEventListener("input", debounce(onTextInput, 300));
+
+function onTextInput(e) {
+    const inputValue = e.target.value;
+    if (inputValue === "") {
+        return;
+    };
+    fetchCountries(inputValue)
+        .then((list) => renderCountries(list))
+        .catch((error) => console.log(error));
+}
+
+function renderCountries(countries) {
+    console.log(countries);
+    if (countries.length > 10) {
+        Notiflix.Notify.failure("Too many matches found. Please enter a more specific name.");
+    };
+    if (countries.length >= 2 & countries.length <= 10) {
+        const countryEl = countries.map(country => {
+            const countryName = country.name.official;
+            const countryFlag = country.flags.svg;
+            return { countryName, countryFlag }
+        });
+        console.log("find",countryEl);
+    }
+}
